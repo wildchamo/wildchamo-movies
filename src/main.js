305c62c014bda7b90954de8b3d4ab5ba2e453289
Jose Luis <wildchamo@gmail.com>
2023-04-26 18:14:44 -0500

    llamo al endpoint de la api y muestro el resultado en consola

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import { API_KEY } from "./dev.js";
 
+async function getTrendingMoviesPreview() {
+
+  const res =await fetch(
+    "https://api.themoviedb.org/3/trending/movie/week?api_key=" + API_KEY
+  );
+  const data = await res.json();
+  
+  const movies = data.results;
+
+  console.log(movies);
+
+}
+getTrendingMoviesPreview()
